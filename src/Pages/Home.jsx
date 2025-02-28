97bac91930dc91c79e8bb9c3f552c2fc58b39b6c
ANU-2524 <anusoni25.2006@gmail.com>
2025-02-28 10:16:44 +0530

    Updated with Basic contact details

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+// src/Home.jsx
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to my React-App</h1>
+      <p>Contact Details:</p>
+      <h3>
+        <h4>Name: Anu</h4>
+        <h4>Email: anu.s84@kalviumcommunity</h4>
+        
+      </h3>
+    </div>
+  );
+};
+
+export default Home;
